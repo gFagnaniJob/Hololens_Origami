9f3832ab4431a97501eefb11e987cf585ab60a17
Giuseppe Fagnani <giuseppef@nautes-ke.it>
2018-08-03 18:22:21 +0200

    intero progetto, da sistemare scritta distanza

---
+++ Origami/Assets/Scripts/CambiaTesto.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CambiaTesto : MonoBehaviour {
+    public Text testoDistanza;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit info = GameObject.FindGameObjectWithTag("Player").GetComponent<WorldCursor>().hitInfo;
+        testoDistanza.text = info.distance.ToString();
+	}
+}
